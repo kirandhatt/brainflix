0f6e4d619e78c8cd983186092c14e0d7723dcc46
kirandhatt <169182085+kirandhatt@users.noreply.github.com>
2024-06-20 15:36:29 -0700

    reduce wrappers

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,8 +2,8 @@ import Main from '../../components/Main/main'
 
 export default function Home() {
     return (
-        <div className="home">
+        <>
             <Main />
-        </div>
+        </>
     )
 }
